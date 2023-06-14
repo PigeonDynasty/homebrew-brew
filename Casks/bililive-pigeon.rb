4481d14509ee757e643d0066d5af2dc67a685551
takenokos <takenokos@users.noreply.github.com>
2023-06-14 06:03:45 +0000

    Deploying to main from @ PigeonDynasty/bililive-pigeon-tauri@2f002329756fef5f7356d134229b2dd038bb191a 🚀

--- Casks/bililive-pigeon.rb
+++ Casks/bililive-pigeon.rb
@@ -1,7 +1,7 @@
 cask "bililive-pigeon" do
   arch arm: "aarch64", intel: "x64"
   version "0.0.3"
-  sha256  intel: ""e595414e2137fa082d00d3921889350d6d7439f9c6ac1d784d0fa9bcbbd56e4a"",
+  sha256  intel: "e595414e2137fa082d00d3921889350d6d7439f9c6ac1d784d0fa9bcbbd56e4a",
           arm:   "20e49d5f01ddb26ac52810f6fb59d63186929eeb0b19f5fdfcef407cc3650e0b"
           
   url "https://github.com/PigeonDynasty/bililive-pigeon-tauri/releases/download/v#{version}/Bililive.Pigeon_#{version}_#{arch}.dmg"
