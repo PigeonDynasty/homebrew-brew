120ddc6e43e0d3272c14d4fd0e70d891286a6285
takenokos <takenokos0916@outlook.com>
2026-08-03 15:36:57 +0800

    update meatshell

--- Casks/meatshell.rb
+++ Casks/meatshell.rb
@@ -1,9 +1,13 @@
 cask "meatshell" do
   version "v0.6.9"
-  sha256 "b9a865541589567c6a05332ebe92a9ec391c74250e5dbe7012369ec34ccee0c6"
-          
-  on_macos do
-    url "https://github.com/jeff141/meatshell/releases/download/#{version}/meatshell-#{version}-macos-aarch64.zip"
+  on_arm do
+    sha256 "b9a865541589567c6a05332ebe92a9ec391c74250e5dbe7012369ec34ccee0c6"
+    url "https://github.com/yituorou/meatshell/releases/download/#{version}/meatshell-#{version}-macos-aarch64.zip"
+  end
+
+  on_intel do
+    sha256 "9bca553506cc6d15b2f6e5d14bcdceb1237ca710c1940fee78c708795da3c7a5"
+    url "https://github.com/yituorou/meatshell/releases/download/#{version}/meatshell-#{version}-macos-x86_64.zip"
   end
   
   livecheck do
