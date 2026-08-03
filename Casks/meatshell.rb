9559e0fbfbcea5e2def950689f46438d707a78e4
takenokos <takenokos0916@outlook.com>
2026-08-03 14:46:43 +0800

    fix: meatshell auto

--- Casks/meatshell.rb
+++ Casks/meatshell.rb
@@ -3,7 +3,7 @@ cask "meatshell" do
   sha256 "b9a865541589567c6a05332ebe92a9ec391c74250e5dbe7012369ec34ccee0c6"
           
   on_macos do
-    url "https://github.com/jeff141/meatshell/releases/download/v#{version}/meatshell-v#{version}-macos-aarch64.zip"
+    url "https://github.com/jeff141/meatshell/releases/download/#{version}/meatshell-#{version}-macos-aarch64.zip"
   end
   
   livecheck do
