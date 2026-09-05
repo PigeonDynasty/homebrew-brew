7ce8cd6f433c63dfb9808d6d6fa6ff70b0e4a932
takenokos <8255049+takenokos@users.noreply.github.com>
2026-09-05 07:44:32 +0000

    chore: update meatshell to v0.7.2

--- Casks/meatshell.rb
+++ Casks/meatshell.rb
@@ -1,12 +1,12 @@
 cask "meatshell" do
-  version "v0.7.1"
+  version "v0.7.2"
   on_arm do
-    sha256 "cfc3c52d44e2732c43f96151f20b1553fc41aae2a00c782e3f1cd439ad817fcb"
+    sha256 "c9607f62da7ec026317054bff1c8622d0f7e127627fcc1112978a7260cf3898a"
     url "https://github.com/yituorou/meatshell/releases/download/#{version}/meatshell-#{version}-macos-aarch64.zip"
   end
 
   on_intel do
-    sha256 "f1c6ff403a3a5899735671fe14a12f7e1e35f3ca312afd52dde93c0509293bb3"
+    sha256 "507376e819683d4b66b459c3c4baf88b67b43a298ccd4ec54250ef54d6d7432b"
     url "https://github.com/yituorou/meatshell/releases/download/#{version}/meatshell-#{version}-macos-x86_64.zip"
   end
   
