6c6f9942731d8a0b2f8b61e889830fa7fe179b5e
takenokos <takenokos0916@outlook.com>
2026-09-06 09:50:45 +0800

    fix: meatshell

--- Casks/meatshell.rb
+++ Casks/meatshell.rb
@@ -1,34 +1,35 @@
 cask "meatshell" do
   version "v0.7.2"
+
   on_arm do
     sha256 "c9607f62da7ec026317054bff1c8622d0f7e127627fcc1112978a7260cf3898a"
+
     url "https://github.com/yituorou/meatshell/releases/download/#{version}/meatshell-#{version}-macos-aarch64.zip"
   end
-
   on_intel do
     sha256 "507376e819683d4b66b459c3c4baf88b67b43a298ccd4ec54250ef54d6d7432b"
+
     url "https://github.com/yituorou/meatshell/releases/download/#{version}/meatshell-#{version}-macos-x86_64.zip"
   end
-  
-  livecheck do
-    url :url
-  end
-
-  auto_updates true
 
   name "meatshell"
   desc "一个轻量级、低内存占用的 SSH / 终端客户端（A lightweight, low-memory SSH / terminal client）"
   homepage "https://github.com/jeff141/meatshell"
 
-  depends_on macos: :catalina
+  livecheck do
+    url :url
+  end
+
+  auto_updates true
+  depends_on :macos
 
   app "meatshell.app"
 
-  postflight do
-    system "sudo", "xattr", "-r", "-d", "com.apple.quarantine", 
-           "/Applications/meatshell.app"
+  postflight_steps do
+    run "sudo",
+        args: ["xattr", "-r", "-d", "com.apple.quarantine",
+               "/Applications/meatshell.app"]
   end
-         
+
   caveats "postflight script already executed quarantine removal."
-  
 end
