69ddd4fe142035a833ea7e967cd34869acd805da
takenokos <takenokos0916@outlook.com>
2026-09-06 09:54:37 +0800

    fix: bililive-pigeon

--- Casks/bililive-pigeon.rb
+++ Casks/bililive-pigeon.rb
@@ -1,30 +1,29 @@
 cask "bililive-pigeon" do
   arch arm: "aarch64", intel: "x64"
+
   version "0.0.5"
-  sha256  intel: "e2c1c94dc941e57415ec022e9b30bc332f7acd06f07df5827a6cdef0d5295cbb",
-          arm:   "e70a8a0c8c69081a3283e3c34ec801b6bbfb0ea902b789ff54d498b5d987498e"
-          
+  sha256 arm:   "e70a8a0c8c69081a3283e3c34ec801b6bbfb0ea902b789ff54d498b5d987498e",
+         intel: "e2c1c94dc941e57415ec022e9b30bc332f7acd06f07df5827a6cdef0d5295cbb"
+
   url "https://github.com/PigeonDynasty/bililive-pigeon-tauri/releases/download/v#{version}/Bililive.Pigeon_#{version}_#{arch}.dmg"
-  
+  name "Bililive Pigeon"
+  desc "Bililive danmaku clirnt"
+  homepage "https://github.com/PigeonDynasty/bililive-pigeon-tauri"
+
   livecheck do
     url :url
   end
 
   auto_updates true
-
-  name "Bililive Pigeon"
-  desc "bililive pigeon."
-  homepage "https://github.com/PigeonDynasty/bililive-pigeon-tauri"
-
-  depends_on macos: :catalina
+  depends_on :macos
 
   app "Bililive Pigeon.app"
-  
-  postflight do
-    system "sudo", "xattr", "-r", "-d", "com.apple.quarantine", 
-           "/Applications/Bililive Pigeon.app"
-  end
-         
-  caveats "postflight script already executed quarantine removal."
 
+  postflight_steps do
+    run "sudo",
+        args: ["xattr", "-r", "-d", "com.apple.quarantine",
+               "/Applications/Bililive Pigeon.app"]
+  end
+
+  caveats "postflight script already executed quarantine removal."
 end
