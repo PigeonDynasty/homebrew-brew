e961eae38638b3360dc79228df974aafcb467fca
takenokos <takenokos0916@outlook.com>
2026-09-06 09:41:38 +0800

    fix: mrrss

--- Casks/mrrss.rb
+++ Casks/mrrss.rb
@@ -1,32 +1,32 @@
 cask "mrrss" do
-  version "v1.3.28"
+  on_system :catalina, :or_newer do
+    version "v1.3.28"
+  end
   sha256 "7f178a6d0cb2741a4bae85b058313ca4f4946c103c3c34625630f70f6f8e286a"
-          
+
   on_macos do
     clean_version = version.sub(/^v/, "")
-    url "https://github.com/WCY-dt/MrRSS/releases/download/#{version}/MrRSS-#{clean_version}-darwin-universal.dmg",
-        verified: "https://github.com/WCY-dt/MrRSS"
+    url "https://github.com/WCY-dt/MrRSS/releases/download/#{version}/MrRSS-#{clean_version}-darwin-universal.dmg"
   end
-  
+
+  name "MrRSS"
+  desc "Modern, cross-platform, and free AI RSS reader. 一个现代化、跨平台且免费的 AI RSS 阅读器"
+  homepage "https://mrrss.ch3nyang.top/"
+
   livecheck do
     url :url
   end
 
   auto_updates true
-
-  name "MrRSS"
-  desc "A modern, cross-platform, and free AI RSS reader. 一个现代化、跨平台且免费的 AI RSS 阅读器."
-  homepage "https://mrrss.ch3nyang.top/"
-
-  depends_on macos: :catalina
+  depends_on :macos
 
   app "MrRSS.app"
 
-  postflight do
-    system "sudo", "xattr", "-r", "-d", "com.apple.quarantine", 
-           "/Applications/MrRSS.app"
+  postflight_steps do
+    run "sudo",
+        args: ["xattr", "-r", "-d", "com.apple.quarantine",
+               "/Applications/MrRSS.app"]
   end
-         
+
   caveats "postflight script already executed quarantine removal."
-  
 end
